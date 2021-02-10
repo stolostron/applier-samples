a706ec0ab4b43e1cff6a3d1d5f1e838283d3e5be
Chris Ahl <cahl@redhat.com>
2021-02-10 15:13:48 -0500

    add support for VMware

--- create-cluster/README.md
+++ create-cluster/README.md
@@ -1,6 +1,6 @@
-# Create and import a cluster on AWS, AZURE or GCP
+# Create and import a cluster on AWS, AZURE, GCP or vSphere
 
-The scriptss [deploy.sh](./hub.sh) uses the [applier](https://github.com/open-cluster-management/library-go/blob/master/docs/applier.md) to apply a number of yamls on the hub in order to create a cluster and then import it as a managed cluster.
+The script [deploy.sh](./deploy.sh) uses the [applier](https://github.com/open-cluster-management/library-go/blob/master/docs/applier.md) to apply a number of yamls on the hub in order to create a cluster and then import it as a managed cluster.
 
 1. Make this directory your current directory
 2. Create a values.yaml based on [values-template.yaml](./values-template.yaml) by setting your cluster name and your options for the different addons.
--- create-cluster/deploy.sh
+++ create-cluster/deploy.sh
@@ -2,7 +2,7 @@
 set -e
 #set -x
 while getopts o:i:dv:h flag
-do 
+do
   case "${flag}" in
     o) OUT="-o ${OPTARG}";;
     i) IN="-values ${OPTARG}";;
@@ -27,23 +27,24 @@ then
 fi
 PARAMS="$(applier -d params.yaml $IN -o /dev/stdout -s)"
 CLOUD=$(echo "$PARAMS" | grep "cloud:" | cut -d ":" -f2 | sed 's/^ //')
-if [ $CLOUD != "aws" ] && [ $CLOUD != "azure" ] && [ $CLOUD != "gcp" ]
-then 
-   echo -e $CLOUD" not supported\nOnly aws, azure and gcp are supported"
+if [ $CLOUD != "aws" ] && [ $CLOUD != "azure" ] && [ $CLOUD != "gcp" ]  && [ $CLOUD != "vsphere" ]
+then
+   echo -e $CLOUD" not supported\nOnly aws, azure, gcp and vsphere are supported"
    exit 1
 fi
 NAME=$(echo "$PARAMS" | grep "name:" | cut -d ":" -f2 | sed 's/^ //')
-if [ -z ${CLOUD+x} ] 
+if [ -z ${CLOUD+x} ]
 then
   echo "Missing cloud type in value.yaml"
   exit 1
 fi
-if [ -z ${NAME+x} ] 
+if [ -z ${NAME+x} ]
 then
   echo "Missing cluster name in value.yaml"
   exit 1
 fi
 
+# Before deploy, make sure namespace does not exist already
 if [ -z ${DEL+x} ]
 then
   set +e
@@ -63,10 +64,17 @@ installConfig:
 $(applier -d hub/$CLOUD/install_config.yaml $IN -o /dev/stdout -s | sed 's/^/  /')
 EOF)
 
+# Create the managed cluster
 if [ -z ${DEL+x} ]
 then
-    echo "$EXT_VALUES" | applier -d hub/common $IN $OUT -s $VERBOSE
+  # vSphere needs an extra secret for CA cert
+  echo "$EXT_VALUES" | applier -d hub/common $IN $OUT -s $VERBOSE
+  if [ ${CLOUD} == "vsphere" ]
+  then
+    echo "$EXT_VALUES" | applier -d hub/vsphere/vsphere_ca_cert_secret_cr.yaml $IN ${OUT} -s $VERBOSE
+  fi
+# Delete the managed cluster
 else
-    echo "$EXT_VALUES" | applier -d hub/common/managed_cluster_cr.yaml $IN $DEL $OUT $VERBOSE
-    echo "$EXT_VALUES" | applier -d hub/common/cluster_deployment_cr.yaml -$IN $DEL $OUT $VERBOSE
-fi 
+  echo "$EXT_VALUES" | applier -d hub/common/managed_cluster_cr.yaml $IN $DEL $OUT $VERBOSE
+  echo "$EXT_VALUES" | applier -d hub/common/cluster_deployment_cr.yaml -$IN $DEL $OUT $VERBOSE
+fi
